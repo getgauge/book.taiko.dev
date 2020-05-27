b454987d6edcc4207bd38e34b3ffed172520726d
Zabil Cheriya Maliackal <zabilcm@gmail.com>
2020-05-27 16:25:10 +0100

    Render chapters from json
    Signed-off-by: Zabil Cheriya Maliackal <zabilcm@gmail.com>

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,14 @@
+let njk = require("nunjucks");
+
 module.exports = function(eleventyConfig) {
   // Output directory: _site
 
   eleventyConfig.addPassthroughCopy("assets");
+
+  // This filter is used for deriving the chapter names
+  // from data in chapters.json
+  eleventyConfig.addFilter('heading', function(str){
+    return str.replace(/_/g, " ");
+  });
+
 };
