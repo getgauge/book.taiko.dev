e1975fcabf168ca422371c9f564144d0370e8975
Zabil Cheriya Maliackal <zabilcm@gmail.com>
2020-05-28 13:33:48 +0100

    Add toc and anchors for sections
    Signed-off-by: Zabil Cheriya Maliackal <zabilcm@gmail.com>

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
-let njk = require("nunjucks");
-
+const tocPlugin = require('eleventy-plugin-toc');
+const markdownIt = require("markdown-it");
+ 
 module.exports = function(eleventyConfig) {
   // Output directory: _site
 
@@ -10,5 +11,19 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addFilter('heading', function(str){
     return str.replace(/_/g, " ");
   });
-
+  let markdownItAnchor = require("markdown-it-anchor");
+  let mdIt = markdownIt({
+		html: true,
+		breaks: true,
+		linkify: true
+	})
+	.use(markdownItAnchor, {
+		permalink: true,
+		permalinkBefore: false,
+		permalinkClass: "direct-link",
+		permalinkSymbol: "#",
+		level: [1,2,3,4]
+	});
+	eleventyConfig.setLibrary("md", mdIt);
+  eleventyConfig.addPlugin(tocPlugin);
 };
